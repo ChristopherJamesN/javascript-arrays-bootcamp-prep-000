da554eeed7cd1543293fa4f382528a7d7caa3931
Chris Nady <nady.christopher@gmail.com>
2017-08-22 15:52:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,18 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (arr, elm) {
+  arr.unshift(elm)
+  return arr
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, elm) {
+  
+}
+
+function addElementToEndOfArray (arr, elm) {
+  
+}
+
+function destructivelyAddElementToEndOfArray (arr, elm) {
+  
+}
